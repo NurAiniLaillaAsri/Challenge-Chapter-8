683b1a986448a585ef7849d0ce71093ac624b737
Nur Aini Lailla Asri <laillaasri@gmail.com>
2022-06-03 14:36:31 +0700

    [Push] deploy

--- config/database.js
+++ config/database.js
@@ -7,28 +7,43 @@ const {
 } = process.env;
 
 module.exports = {
+  // development: {
+  //   username: DB_USER,
+  //   password: DB_PASSWORD,
+  //   database: `${DB_NAME}_development`,
+  //   host: DB_HOST,
+  //   port: DB_PORT,
+  //   dialect: "postgres"
+  // },
+  // test: {
+  //   username: DB_USER,
+  //   password: DB_PASSWORD,
+  //   database: `${DB_NAME}_test`,
+  //   host: DB_HOST,
+  //   port: DB_PORT,
+  //   dialect: "postgres"
+  // },
+  // production: {
+  //   username: DB_USER,
+  //   password: DB_PASSWORD,
+  //   database: `${DB_NAME}_production`,
+  //   host: DB_HOST,
+  //   port: DB_PORT,
+  //   dialect: "postgres"
+  // }
+
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+    }
   },
-  test: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
-  },
-  production: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
-  }
 }
